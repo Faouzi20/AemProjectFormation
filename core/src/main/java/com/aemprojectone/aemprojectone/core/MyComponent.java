e56f1174d8853d19f4eafc81b901b2e08d48acd3
sfn <elabbadisoufiane@gmail.com>
2018-03-06 14:18:23 +0000

    getResource attributes by Soufiane

--- core/src/main/java/com/aemprojectone/aemprojectone/core/MyComponent.java
+++ core/src/main/java/com/aemprojectone/aemprojectone/core/MyComponent.java
@@ -1,6 +1,18 @@
 package com.aemprojectone.aemprojectone.core;
 
+
+
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.ValueFormatException;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.slf4j.Logger;
+
 import com.adobe.cq.sightly.WCMUsePojo;
+import com.adobe.cq.social.calendar.client.api.Page;
 
 
 public class MyComponent extends WCMUsePojo {
@@ -8,15 +20,30 @@ public class MyComponent extends WCMUsePojo {
 	private String text;
 	private String pathTitle;
 	private String typeText;
-	
-	
+	private String pathbrowser;
+	private String titleComp;
+	private ResourceResolver resourceResolver;
 
 	@Override
-	public void activate() {
+	public void activate() {//throws ValueFormatException, PathNotFoundException, RepositoryException {
 		myTitle = "My Project " + getCurrentPage().getTitle();
 		text =  (String) getProperties().get("text", "toto");
 		pathTitle = getProperties().get("pathbrowser", "test");
 		typeText = getProperties().get("type", "size");
+		pathbrowser = getProperties().get("pathbrowser", "size");
+		/*ResourceResolver resourceResolver = getResourceResolver();
+	    Resource res = resourceResolver.getResource(pathbrowser);
+		Node node = resourceResolver.adaptTo(Node.class);*/
+		
+		Resource pageResource = getResourceResolver().getResource(pathbrowser);
+		titleComp = pageResource.getName();
+		//Node node = pageResource.adaptTo(Node.class);
+		//Node node = pageResource.adaptTo(Node.class);
+		//titleComp = node.getPath();
+		//titleComp = resourceResolver.getResource(pathbrowser).getName();
+//		Node node = resourceResolver.getResource(pathbrowser).adaptTo(Node.class);
+	//	titleComp = node.hasProperty("title") ? node.getProperty("title").getString() : "default title";
+		//titleComp = "Default";
 	}
 	
 	public String getMyTitle() {
@@ -33,6 +60,14 @@ public class MyComponent extends WCMUsePojo {
 	
 	public String getTypeText() {
 		return typeText;
+	}
+
+	public String getPathbrowser() {
+		return pathbrowser;
+	}
+
+	public String getTitleComp() {
+		return titleComp;
 	}
 	
 }
