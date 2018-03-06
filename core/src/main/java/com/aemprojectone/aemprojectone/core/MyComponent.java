292bf5536da9ac979d11b232d596a155cbc34aba
DESKTOP-M5BHTB9\user <faouzi.edrissi@gmail.com>
2018-03-06 09:28:18 +0000

    Component Helloworld modified

--- core/src/main/java/com/aemprojectone/aemprojectone/core/MyComponent.java
+++ core/src/main/java/com/aemprojectone/aemprojectone/core/MyComponent.java
@@ -6,17 +6,33 @@ import com.adobe.cq.sightly.WCMUsePojo;
 public class MyComponent extends WCMUsePojo {
 	private String myTitle;
 	private String text;
+	private String pathTitle;
+	private String typeText;
 	
+	
+
 	@Override
 	public void activate() {
 		myTitle = "My Project " + getCurrentPage().getTitle();
 		text =  (String) getProperties().get("text", "toto");
-		}
+		pathTitle = getProperties().get("pathbrowser", "test");
+		typeText = getProperties().get("type", "size");
+	}
+	
 	public String getMyTitle() {
 		return myTitle;
-		}
+	}
+	
 	public String getText() {
 		return text;
 	}
-	
+
+	public String getPathTitle() {
+		return pathTitle;
 	}
+	
+	public String getTypeText() {
+		return typeText;
+	}
+	
+}
