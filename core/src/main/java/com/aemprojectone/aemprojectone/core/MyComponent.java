01664384122d242f3b6a75c363236c2d19b06343
sfn <elabbadisoufiane@gmail.com>
2018-03-07 18:28:32 +0000

    breadCrumb add succesfully

--- core/src/main/java/com/aemprojectone/aemprojectone/core/MyComponent.java
+++ core/src/main/java/com/aemprojectone/aemprojectone/core/MyComponent.java
@@ -1,17 +1,11 @@
 package com.aemprojectone.aemprojectone.core;
-
-import javax.jcr.Node;
-import javax.jcr.PathNotFoundException;
-import javax.jcr.RepositoryException;
-import javax.jcr.ValueFormatException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.slf4j.Logger;
-
 import com.adobe.cq.sightly.WCMUsePojo;
-import com.adobe.cq.social.calendar.client.api.Page;
-
+import com.day.cq.wcm.api.Page;
+import com.day.durbo.DurboInput.Node;
 
 public class MyComponent extends WCMUsePojo {
 	private String myTitle;
@@ -22,22 +16,24 @@ public class MyComponent extends WCMUsePojo {
 	private String pathbrowser;
 	private String titleComp;
 	private String color;
+	private String pageParent;
 
 	@Override
 	public void activate() {
+		
 		myTitle = "My Project " + getCurrentPage().getTitle();
+
 		text =  (String) getProperties().get("text", "toto");
 		pathTitle = getProperties().get("pathbrowser", "test");
 		typeText = getProperties().get("type", "size");
 		pathbrowser = getProperties().get("pathbrowser", "size");
 		textfield =  (String) getProperties().get("textcolor", "default text");
 		color =  (String) getProperties().get("color", "black");
-		
 		Resource pageResource = getResourceResolver().getResource(pathbrowser);
-		titleComp = pageResource.getValueMap().get("jcr:title", String.class);		
-
-	}
+		titleComp = pageResource.getValueMap().get("jcr:title", String.class);
 	
+	}
+
 	public String getMyTitle() {
 		return myTitle;
 	}
@@ -69,6 +65,8 @@ public class MyComponent extends WCMUsePojo {
 	public String getColor() {
 		return color;
 	}
-	
+	public String getPageParent() {
+		return pageParent;
+	}
 	
 }
